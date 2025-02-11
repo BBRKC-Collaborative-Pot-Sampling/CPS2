579aa61bb08c695a940443996a5a84afca21bfe6
Shannon Hennessey <s.hennessey13@gmail.com>
2025-02-11 10:33:28 -0900

    modified script to also calculate count estimates based of representitive basket counts/weights; tightened up code so it's all in one pipe

--- Scripts/CPS2_TRAWL_FISH_processing.R
+++ Scripts/CPS2_TRAWL_FISH_processing.R
@@ -16,9 +16,12 @@
 # LOAD FISH DATA -----------------------------------------------------------------------------------------------------------------
 
   # Set trawl data filepath
-      path <- "Y:/KOD_Survey/CPS2/Data/Trawl Data/Groundfish/" 
+      path <- "Y:/KOD_Survey/CPS2/Trawl Data/Groundfish/" 
 
   # Load summary catch tables to get full species list
+      # basically just using 'catch' files to assign species code because there aren't
+      # any haul identifiers in each file -- unless you go haul by haul with all files
+      # this doesn't work for any batch processing...
       catch <- list.files(paste0(path, "Catch - FTP/")) %>%
                purrr::map_df(~read.csv(paste0(path, "Catch - FTP/", .x))) %>%
                dplyr::select(SPECIES_CODE, SPECIES_NAME) %>%
@@ -37,65 +40,86 @@
       
       
   # Read in haul data
-      hauls <- read.csv("Y:/KOD_Survey/CPS2/Data/Trawl Data/VA_HAULS.csv") %>% # Vesteraalen
+      hauls <- read.csv("Y:/KOD_Survey/CPS2/Trawl Data/VA_HAULS.csv") %>% # Vesteraalen
                dplyr::filter(SAMPLED == 1)
 
 
 # PROCESS FISH DATA ----------------------------------------------------------------------------------------------------------------
   # Join raw_sample_values and raw_sample to get total weight per species per haul
-      samples <- left_join(raw_sample_values %>% select(CATCH_SAMPLE_VALUE_ID, CATCH_SAMPLE_ID, SAMPLE_VALUE_WEIGHT, SAMPLE_VALUE_WEIGHT_UNITS,
-                                                          SAMPLE_VALUE_COUNT, KEEP_FLAG, RECORDING_DEVICE, RECORDER), 
-                             raw_sample %>% select(HAUL_ID, CATCH_SAMPLE_ID, SPECIES_CODE, RECORDING_DEVICE, RECORDER), 
-                             by = c("CATCH_SAMPLE_ID", "RECORDING_DEVICE", "RECORDER")) %>%
-                   left_join(., raw_haul %>% select(HAUL_ID, CRUISE_ID, HAUL, STATION, RECORDING_DEVICE)) %>%
-                   # remove crab from samples
-                   dplyr::filter(!SPECIES_CODE %in% c(68560, 69322, 69400)) %>%
-                   # join with catch to get species names
-                   left_join(., catch) %>%  
-                   # filter out weight corrections
-                   filter(if_all(c(HAUL_ID, SPECIES_CODE), complete.cases)) %>%
-                   # calculate weight totals for each species
-                   dplyr::group_by(HAUL, HAUL_ID, CATCH_SAMPLE_ID, SPECIES_CODE, SPECIES_NAME, RECORDING_DEVICE) %>%
-                   dplyr::summarise(WEIGHT = sum(SAMPLE_VALUE_WEIGHT))
-      
-                   # *Might have to re-incorporate this bit of code to deal with counts??
-                   # distinct() %>%
-                   # dplyr::left_join(samples %>% select(-c(SAMPLE_VALUE_WEIGHT, CATCH_SAMPLE_VALUE_ID))) %>%
-                   # distinct() %>%
-                   # dplyr::select(HAUL, HAUL_ID, CATCH_SAMPLE_ID, WEIGHT, SPECIES_CODE, SPECIES_NAME, RECORDING_DEVICE) %>%
-                    
-
-  # Calculate sampling factor from samples, join back with samples file to 
-  # get specimen information, join with raw_haul file to get haul and station #s, join with hauls
-  # file to get lat/lon, set/haul date and time for each haul (with positive catch)
-      specimen_table <- samples %>%
-                        # dplyr::group_by(HAUL, HAUL_ID, CATCH_SAMPLE_ID, RECORDING_DEVICE) %>%
-                        dplyr::reframe(FLATFISH = ifelse(SPECIES_CODE %in% c(10210, 10220, 10261, 10285, 10110, 10130,
-                                                                             10269, 10211, 10112, 10270, 10200), 1, 0)) %>%
-                        right_join(., samples) %>%
-                        dplyr::group_by(HAUL, HAUL_ID, RECORDING_DEVICE) %>%
-                        dplyr::mutate(IDENT = sum(WEIGHT[FLATFISH == 1]), across(),
-                                      UNIDENT = sum(WEIGHT[SPECIES_NAME == "flatfish unid."]), across(),
-                                      SAMPLING_FACTOR = (IDENT + UNIDENT)/IDENT) %>%
+  # Expand unidentified/subsampled flatfish weights to ID'd flatfish
+  # Estimate counts based on basket count
+  # Expand counts and weights by sampling factor
+  # Join with haul info, format final output
+      specimen_table <- left_join(raw_sample_values %>% select(CATCH_SAMPLE_VALUE_ID, CATCH_SAMPLE_ID, SAMPLE_VALUE_WEIGHT, SAMPLE_VALUE_WEIGHT_UNITS,
+                                                               SAMPLE_VALUE_COUNT, KEEP_FLAG, RECORDING_DEVICE, RECORDER), 
+                                  raw_sample %>% select(HAUL_ID, CATCH_SAMPLE_ID, SPECIES_CODE, RECORDING_DEVICE, RECORDER), 
+                                  by = c("CATCH_SAMPLE_ID", "RECORDING_DEVICE", "RECORDER")) %>%
+                        left_join(., raw_haul %>% select(HAUL_ID, CRUISE_ID, HAUL, STATION, RECORDING_DEVICE)) %>%
+                        # remove crab from samples
+                        dplyr::filter(!SPECIES_CODE %in% c(68560, 69322, 69400)) %>%
+                        # join with catch to get species names
+                        left_join(., catch) %>%  
+                        # rename "Pacific cod (adult)" --> "Pacific cod"
+                        mutate(SPECIES_NAME = ifelse(SPECIES_NAME == "Pacific cod (adult)", "Pacific cod", SPECIES_NAME),
+                               SPECIES_CODE = ifelse(SPECIES_CODE == 21722, 21720, SPECIES_CODE)) %>%
+                        # filter out weight corrections
+                        # not sure how to handle these b/c they have no species identifier
+                        filter(if_all(c(HAUL_ID, SPECIES_CODE), complete.cases)) %>%
+                        # parse unidentified flatfish weights to respective species
+                        # arrowtooth, Kam, flathead, rex, yellowfin, longhead, starry, rock, butter, plaice 
+                        group_by(STATION) %>%
+                        mutate(FLATFISH = ifelse(SPECIES_CODE %in% c(10210, 10220, 10261, 10285, 10110, 10130,
+                                                                     10211, 10112, 10270, 10200), 1, 0),
+                               IDENT = sum(SAMPLE_VALUE_WEIGHT[FLATFISH == 1]), 
+                               UNIDENT = sum(SAMPLE_VALUE_WEIGHT[SPECIES_NAME == "flatfish unid."]),
+                               SAMPLING_FACTOR = (IDENT + UNIDENT)/IDENT, 
+                               # set sampling factor to 1 if not a flatfish
+                               SAMPLING_FACTOR = ifelse(!SPECIES_CODE %in% c(10210, 10220, 10261, 10285, 10110, 10130,
+                                                                             10211, 10112, 10270, 10200), 1, SAMPLING_FACTOR)) %>%
                         # remove unidentified flatfish bin
                         dplyr::filter(!SPECIES_CODE == 10001) %>% 
-                        # set sampling factor to 1 if not a flatfish
-                        dplyr::mutate(SAMPLING_FACTOR = ifelse(!SPECIES_CODE %in% c(10210, 10220, 10261, 10285, 10110, 10130,
-                                                                                   10269, 10211, 10112, 10270, 10200), 1, SAMPLING_FACTOR)) %>%
-                        dplyr::left_join(., raw_haul, by = c("HAUL", "HAUL_ID")) %>%
-                        dplyr::left_join(., hauls %>% select(STATION, LAT_DD, LON_DD)) %>%
-                        dplyr::mutate(VESSEL = "Vesteraalen", 
-                                      CRUISE = 202301) %>% 
-                        dplyr::select(CRUISE, VESSEL, HAUL, STATION, LAT_DD, LON_DD, #DATE_HAUL, TIME_HAUL, SOAK_TIME, DEPTH_F,
-                                      SPECIES_CODE, SPECIES_NAME, WEIGHT, SAMPLING_FACTOR) %>%
-                        dplyr::mutate(CALC_WEIGHT = WEIGHT*SAMPLING_FACTOR)
-      
+                        select(-IDENT, -UNIDENT, -FLATFISH) %>%
+                        # expand basket count to rest of sample
+                        mutate(IND_WEIGHT_CALC = SAMPLE_VALUE_WEIGHT/SAMPLE_VALUE_COUNT) %>%
+                        group_by(HAUL, HAUL_ID, CATCH_SAMPLE_ID, RECORDING_DEVICE, STATION, 
+                                 SPECIES_CODE, SPECIES_NAME, SAMPLING_FACTOR) %>%
+                        nest() %>%
+                        mutate(data = map(data, function(df){
+                          # pull individual fish weight
+                          ind_weight <- unique(na.omit(df$IND_WEIGHT_CALC))
+                          # if more than one basket counted, take mean of the calculated individual weight
+                          if(length(ind_weight) > 1) ind_weight <- mean(ind_weight)
+                          # calculate estimated counts from weight
+                          counts <- df %>%
+                                    {if(length(ind_weight) == 1) mutate(., COUNT = SAMPLE_VALUE_WEIGHT/ind_weight,
+                                                                           IND_WEIGHT = ind_weight) 
+                                      else(mutate(., COUNT = NA, IND_WEIGHT = NA))}
+                          left_join(df, counts)
+                        })) %>%
+                        unnest(cols = c(data)) %>%
+                        # calculate weight and count totals for each species
+                        summarise(IND_WEIGHT = mean(IND_WEIGHT),
+                                  WEIGHT = sum(SAMPLE_VALUE_WEIGHT), 
+                                  COUNT = sum(COUNT)) %>%
+                        left_join(., hauls %>% select(STATION, LAT_DD, LON_DD)) %>%
+                        mutate(VESSEL = "Vesteraalen", 
+                               CRUISE = 202301) %>%
+                        mutate(CALC_WEIGHT = WEIGHT*SAMPLING_FACTOR, 
+                               CALC_COUNT = COUNT*SAMPLING_FACTOR) %>%
+                        ungroup() %>%
+                        select(CRUISE, VESSEL, HAUL, STATION, LAT_DD, LON_DD, #DATE_HAUL, TIME_HAUL, SOAK_TIME, DEPTH_F,
+                               SPECIES_CODE, SPECIES_NAME, IND_WEIGHT, WEIGHT, COUNT,
+                               SAMPLING_FACTOR, CALC_WEIGHT, CALC_COUNT)
+
+        
     # Save .csv
         write.csv(specimen_table, "./Data/CPS2_2024_trawl_fishcatch.csv", row.names = FALSE)
       
         
     # Reframe and aggregate calculated weights by main bycatch species (to match POT bycatch dataframe)
         fish_sum <- specimen_table %>%
+                    # remove non-fish taxa
+                    filter(!SPECIES_NAME %in% c("Octopus sp.", "octopus unid.", "Hanasaki king crab, Male, All Sizes")) %>%
                     dplyr::mutate(SPP_LABS = dplyr::case_when(SPECIES_NAME == "Pacific cod" ~ "PacificCod",
                                                               SPECIES_NAME == "Pacific halibut" ~ "Halibut",
                                                               SPECIES_NAME == "great sculpin" ~ "GreatSculpin", 
@@ -105,7 +129,10 @@
                                                               SPECIES_NAME == "northern rock sole" ~ "RockSole",
                                                               TRUE ~ "Other")) %>%
                     dplyr::group_by(VESSEL, STATION, LAT_DD, LON_DD, SPP_LABS) %>%
-                    dplyr::summarise(WEIGHT = sum(CALC_WEIGHT))
+                    dplyr::summarise(WEIGHT = sum(CALC_WEIGHT),
+                                     COUNT = sum(CALC_COUNT))
+        
+        write.csv(fish_sum, "./Data/CPS2_2024_trawl_fish_station_weightcount.csv", row.names = FALSE)
         
         bycatch_spp <- unique(fish_sum$SPP_LABS)
         
